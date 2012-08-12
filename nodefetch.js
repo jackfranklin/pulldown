3f424ce64f2c82f754585acbad7688e0aae11489
Jack Franklin <jack@jackfranklin.net>
2012-08-12 20:13:57 +0100

    it works

--- nodefetch.js
+++ nodefetch.js
@@ -3,13 +3,33 @@
 //some dependencies
 var exec = require('child_process').exec;
 var url = require('url');
+var fs = require('fs');
+
+var gotPackages = false;
+
+
+var checkForSettings = function() {
+  try {
+    // Query the entry
+    var settings = fs.lstatSync(getUserHome() + '/nodefetch.json');
+    console.log("-> Found settings file");
+    fromSettings();
+  }
+  catch (e) {
+    var url = "http://jackfranklin.org/nodefetch.json"
+    console.log("-> No settings file detected. Downloading default from " + url);
+    wget(url, getUserHome() + "/nodefetch.json", fromSettings);
+  }
+};
 
 
 if(process.argv[2] == "--help") {
-  console.log("Please refer to the Github README for detailed help");
-  console.log("Inline terminal help is coming soon");
+  console.log("-> Please refer to the Github README for detailed help");
+  console.log("-> Inline terminal help is coming soon");
   process.exit(1);
 }
+
+
 
 
 //where we will store the packages we read in from the .nodefetch file
@@ -17,15 +37,16 @@ var packages = {};
 
 //function to pull in the dependencies from the .nodefetch file
 var fromSettings = function() {
-  var fs = require('fs');
-  var packagesArray = fs.readFileSync(getUserHome() + '/.nodefetch').toString().split("\n");
-  for(var i = 0; i < packagesArray.length; i++) {
-    var p = packagesArray[i];
-    if(p !== "") {
-      var spl = p.split("|")
-      packages[spl[0]] = spl[1];
-    }
+  if(!gotPackages) {
+    var fs = require('fs');
+    var packagesJson = fs.readFileSync(getUserHome() + '/nodefetch.json').toString();
+    packages = JSON.parse(packagesJson);
+    gotPackages = true;
   }
+
+  //now we have them, lets get the file
+  getTarget();
+
 };
 
 //utility function to get the home directory that should be platform agnostic
@@ -36,7 +57,7 @@ var getUserHome = function() {
 
 
 //function that executes wget to pull down the file
-var wget = function(fileUrl, output) {
+var wget = function(fileUrl, output, cb) {
   //default to the filename on the server if one is not passed in
   output = output || url.parse(fileUrl).pathname.split('/').pop();
   var wgetCommand = "wget -O " + output + " " + fileUrl;
@@ -44,38 +65,28 @@ var wget = function(fileUrl, output) {
     if(err) {
       throw err;
     } else {
-      console.log(fileUrl + " downloaded to " + output);
+      console.log("-> " + fileUrl + " downloaded to " + output);
     }
+    cb && cb();
   });
 };
 
 //process the command line arguments and perform the wget
 var getTarget = function() {
   var fileUrl = packages[process.argv[2]];
+  if(process.argv[3]) {
+    console.log("-> Attempting to download package", process.argv[2], "to", process.argv[3]);
+  } else {
+    console.log("-> Attempting to download package", process.argv[2]);
+  }
   if(!fileUrl) {
-    throw Error("Package " + fileUrl + " not found");
+    throw Error("-> Package " + fileUrl + " not found");
   } else {
     wget(fileUrl, process.argv[3]);
   }
 };
 
-fromSettings();
-getTarget();
 
-//wget -O blah.js http://backbonejs.org/backbone-min.js
+//lets do this!
 
-/*
- * // Function to download file using wget
-var download_file_wget = function(file_url) {
-
-    // extract the file name
-    var file_name = url.parse(file_url).pathname.split('/').pop();
-    // compose the wget command
-    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + file_url;
-    // excute wget using child_process' exec function
-
-    var child = exec(wget, function(err, stdout, stderr) {
-        if (err) throw err;
-        else console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-    });
-}; */
+checkForSettings();
