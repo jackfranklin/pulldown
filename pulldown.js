860f444b0cdb263c262c35f4c2cad8825e23c8bf
Jack Franklin <jack@jackfranklin.net>
2013-05-05 01:30:08 +0100

    Rewrite to use the resolver and middleman modules

--- pulldown.js
+++ pulldown.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
 //some dependencies
-var URL = require('url'),
-    fs = require('fs'),
-    request = require('request'),
-    shell = require('shelljs'),
-    pkg = require('./package.json'),
-    resolve = require("pulldown-resolve");
+var URL = require('url');
+var fs = require('fs');
+var request = require('request');
+var shell = require('shelljs');
+var pkg = require('./package.json');
+var resolve = require("pulldown-resolve");
+var middleMan = require("pulldown-middle-man");
 
 //terminal output colours!
 //via http://roguejs.com/2011-11-30/console-colors-in-node-js/
-var red   = '\033[31m',
-    green = '\033[32m',
-    reset = '\033[0m';
+var red   = '\033[31m';
+var green = '\033[32m';
+var reset = '\033[0m';
 
 var log = function(message, colour) {
   colour ? console.log("->", colour, message, reset) : console.log("->", message);
@@ -28,8 +29,10 @@ var Pulldown = function() {
 
 Pulldown.prototype.init = function() {
   this.localJson = this.getLocalJson();
-  var res = this.processUserArgs();
-  this.downloadFiles();
+  var self = this;
+  var res = this.processUserArgs(function(urls) {
+    self.downloadFiles(urls);
+  });
 };
 
 Pulldown.prototype.getLocalJson = function() {
@@ -40,63 +43,43 @@ Pulldown.prototype.getLocalJson = function() {
   return file;
 };
 
-Pulldown.prototype.processUserArgs = function() {
+Pulldown.prototype.processUserArgs = function(callback) {
   var self = this;
   var args = process.argv.slice(2);
   args.forEach(function(item) {
-    var parsedUrls = self.parsePackageArgument(item);
-    var outputDestination = item.split(":")[1];
-    if(parsedUrls) {
-      parsedUrls.forEach(function(url) {
-        self.files.push({ url: url, out: outputDestination });
-      });
-    }
+    self.parsePackageArgument(item, function(data) {
+      callback(data);
+    });
   });
   return this.files;
 };
 
-Pulldown.prototype.parsePackageArgument = function(searchTerm) {
-  // look within ~/.pulldown.json first
-  searchTerm = searchTerm.split(":")[0];
-  var resultUrls = [];
-  // pulldown-resolve goes here
-  var urls = resolve(searchTerm, this.localJson);
-  if(urls.length) {
-    resultUrls = resultUrls.concat(urls);
-  } else {
-    // else hit the CDN
-    resultUrls.push(this.cdnUrl(searchTerm));
-  }
-  return resultUrls;
+Pulldown.prototype.parsePackageArgument = function(searchTerm, callback) {
+  resolve(searchTerm, {
+    registry: this.localJson,
+    helper: function(identifier, callback) {
+      middleMan.set(identifier, function(data) {
+        data = data.map(function(item) {
+          return item[0] === "/" ? "https:" + item : item;
+        });
+        callback(null, data);
+      });
+    }
+  }, function(err, set) {
+    callback(set);
+  });
 };
 
-Pulldown.prototype.cdnUrl = function(term) {
-  // hit CDN
-  return "http://code.jquery.com/jquery-1.9.1.js";
-};
-
-Pulldown.prototype.findUrlForString = function(arg) {
-  // string could either be URL, in which case return it
-  // or a search term, in which case try to find the URL
-  // first from local pulldown.json or CDN
-  if(isUrl(arg)) return arg;
-
-  var localRes = this.localJson[arg];
-  if(localRes && isUrl(localRes)) return localRes;
-
-  return this.cdnUrl(arg);
-};
-
-Pulldown.prototype.downloadFiles = function() {
-  this.files.forEach(function(file) {
+Pulldown.prototype.downloadFiles = function(urls) {
+  urls.forEach(function(file) {
     this.getFile(file);
   }.bind(this));
 };
 
-Pulldown.prototype.getFile = function(fileObj) {
-  var fileDestination = fileObj.out || URL.parse(fileObj.url).pathname.split('/').pop();
-  request(fileObj.url).pipe(fs.createWriteStream(fileDestination).on("close", function() {
-    log("Success: " + fileObj.url + " has been downloaded to " + fileDestination, green);
+Pulldown.prototype.getFile = function(url) {
+  var fileDestination = URL.parse(url).pathname.split('/').pop();
+  request(url).pipe(fs.createWriteStream(fileDestination).on("close", function() {
+    log("Success: " + url + " has been downloaded to " + fileDestination, green);
   }));
 };
 
