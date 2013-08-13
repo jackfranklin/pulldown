e237be78a91e5c2f261d79726f6cb708a7d36854
Jack Franklin <jack@jackfranklin.net>
2013-08-13 16:17:21 +0100

    add test for a set

--- test/urls.js
+++ test/urls.js
@@ -50,4 +50,20 @@ describe("Searching for a library", function() {
 
 describe("Searching for a set", function() {
   beforeEach(setup);
+
+  it("calls getFile for each of the files", function(done) {
+    var apiSet = mockAndReturn("backbone", [ "backbone.js", "underscore", "jquery" ]);
+    var apiBackbone = mockAndReturn("backbone.js", [ "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js" ]);
+    var apiUnderscore = mockAndReturn("underscore", [ "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js" ]);
+    var apiJQuery = mockAndReturn("jquery", [ "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js" ]);
+
+    new Pulldown().init(["backbone"], function() {
+      assert(apiSet.isDone(), "the API was hit");
+      assert(spy.calledThrice, "getFile was called 3 times");
+      assert(spy.calledWith("https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"), "getFile was called for Backbone");
+      assert(spy.calledWith("https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js"), "getFile was called for Underscore");
+      assert(spy.calledWith("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"), "getFile was called for jQuery");
+      done();
+    });
+  });
 });
