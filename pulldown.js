69c72625f954b8375adff19ed86b9db25e5ab8be
Jack Franklin <jack@jackfranklin.net>
2013-08-18 12:48:44 +0100

    move help into lib to tidy things up

--- pulldown.js
+++ pulldown.js
@@ -99,25 +99,7 @@ Pulldown.prototype.init = function(userArgs, done) {
   }
 };
 
-Pulldown.prototype.help = function () {
-  console.log();
-  console.log('  Usage: pulldown <identifier>[::<file>] [<identifier>[::<file>], ...] [options]');
-  console.log();
-  console.log('  An <identifier> can be a URL, a library name or a set.');
-  console.log();
-  console.log('  Options:');
-  console.log();
-  console.log('    -o, --output  output directory');
-  console.log();
-  console.log('  Example usage:');
-  console.log();
-  console.log('    pulldown jquery             # Downloads jQuery');
-  console.log('    pulldown jquery::jq.js      # Downloads jQuery to jq.js');
-  console.log('    pulldown jquery angular.js  # Downloads jQuery and Angular.js');
-  console.log('    pulldown backbone           # Downloads jQuery, Underscore.js and Backbone.js');
-  console.log('    pulldown backbone -o js     # Downloads same as above, but into js/');
-  console.log();
-};
+Pulldown.prototype.help = require("./lib/help");
 
 Pulldown.prototype.getLocalJson = function() {
   var file;
