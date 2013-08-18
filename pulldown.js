9f3d6c6dfa5198afe4913234441272a385b72de8
Jack Franklin <jack@jackfranklin.net>
2013-08-18 14:02:20 +0100

    Fix zips not being sent to the right place

--- pulldown.js
+++ pulldown.js
@@ -15,6 +15,7 @@ var _              = require("underscore");
 var chalk          = require("chalk");
 var updateNotifier = require('update-notifier');
 var parseArgs      = require('./lib/parseargs');
+var optimist       = require('optimist');
 
 
 var isUrl = function(str) {
@@ -28,7 +29,9 @@ var Pulldown = function() {
 Pulldown.prototype.init = function(userArgs, done) {
   done = done || function () {};
   var stripBools = parseArgs(userArgs);
-  userArgs = stripBools.parsedArgs;
+  var optimistParsed = optimist.parse(stripBools.parsedArgs);
+  userArgs = optimistParsed._;
+  this.outputDir = optimistParsed.o || optimistParsed.output;
   var bools = stripBools.bools;
   if (!userArgs.length || bools.h || bools.help) return this.help();
 
@@ -59,7 +62,6 @@ Pulldown.prototype.ls = function(done) {
 
 Pulldown.prototype.processDownload = function(userArgs, bools, done) {
   this.isDryRun = bools.d || bools["dry-run"];
-  this.outputDir = bools.o || bools.output;
 
   if(this.isDryRun) {
     this.log("Dry Run - no files will be downloaded", "underline");
@@ -157,7 +159,7 @@ Pulldown.prototype.getFile = function(url, out, doneGetFile) {
   var fileDestination = path.join(this.outputDir || ".", out + (isAZip && needsZip ? '.zip' : ''));
 
   // calculate outpath for zip
-  var outPath = out.replace(/\.zip$/i, '');
+  var outPath = self.zipOutPath(out);
 
   if(this.isDryRun) {
     this.log(url + " would have been downloaded to " + fileDestination, "green");
@@ -185,12 +187,16 @@ Pulldown.prototype.getFile = function(url, out, doneGetFile) {
         fileDestination: fileDestination
       });
     } else {
-      self.extractZip(fileDestination, outPath, doneGetFile);
+      self.extractZip(url, fileDestination, outPath, doneGetFile);
     }
   });
 };
 
-Pulldown.prototype.extractZip = function(file, out, doneGetFile) {
+Pulldown.prototype.zipOutPath = function(out) {
+  return path.join(this.outputDir || ".", out.replace(/\.zip$/i, ''));
+};
+
+Pulldown.prototype.extractZip = function(url, file, out, doneGetFile) {
   var self = this;
   fs.createReadStream(file)
   .pipe(unzip.Extract({ path: out }))
--- test/zips.js
+++ test/zips.js
@@ -1,11 +1,24 @@
-var assert = require("assert");
+var assert   = require("assert");
 var Pulldown = require("../pulldown");
-var nock = require("nock");
-var sinon = require("sinon");
+var nock     = require("nock");
+var sinon    = require("sinon");
+var URL      = require('url');
 var spy, log;
 var oldGetFile = Pulldown.prototype.getFile;
+var oldExtractZip = Pulldown.prototype.extractZip;
 
-var setup = function() {
+var stubGetFileForZip = function() {
+  spy = sinon.spy();
+  Pulldown.prototype.getFile = function(url, out, cb) {
+    out = out || URL.parse(url).pathname.split("/").pop();
+    var outPath = this.zipOutPath(out);
+    this.extractZip(url, out, outPath, cb);
+  };
+
+  Pulldown.prototype.extractZip = function(url, file, out, cb) {
+    spy.apply(this, Array.prototype.slice.call(arguments));
+    return cb(null, { url: url, fileDestination: out, unzipped: true });
+  };
 };
 
 var stubLogs = function() {
@@ -23,10 +36,28 @@ var mockAndReturn = function(searchTerm, result) {
 
 
 describe("Downloading and extracting a zip", function() {
+  beforeEach(stubGetFileForZip);
   it("extracts it to the CWD", function(done) {
-    var api = mockAndReturn("jquery" [ "//cdn//foo.zip" ]);
+    var api = mockAndReturn("jquery", [ "//cdn//foo.zip" ]);
     new Pulldown().init(["jquery"], function() {
+      assert(spy.calledWith(
+        "https://cdn//foo.zip", // url
+        "foo.zip", // file name
+        "foo" // output dir
+      ), "extractZip called with arguments");
+      done();
     });
   });
 
+  it("allows the output dir to be defined", function(done) {
+    var api = mockAndReturn("jquery", [ "//cdn//foo.zip" ]);
+    new Pulldown().init(["jquery", "-o", "bar"], function() {
+      assert(spy.calledWith(
+        "https://cdn//foo.zip", // url
+        "foo.zip", // file name
+        "bar/foo" // output dir
+      ), "extractZip called with arguments");
+      done();
+    });
+  });
 });
