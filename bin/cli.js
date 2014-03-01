44676e672befcdd0f8a49f8dc071e4b17c9576f1
Jack Franklin <jack@jackfranklin.net>
2014-03-01 19:46:22 +0000

    Move help into the CLI. Closes #87

--- bin/cli.js
+++ bin/cli.js
@@ -24,10 +24,35 @@ var cli = {
     message = (colour ? chalk[colour](message) : message);
     console.log(prefix, message);
   },
+  help: function() {
+    console.log();
+    console.log('  Usage: pulldown <identifier>[::<file>] [<identifier>[::<file>], ...] [options]');
+    console.log();
+    console.log('  An <identifier> can be a URL, a library name or a set.');
+    console.log();
+    console.log('  Options:');
+    console.log();
+    console.log('    -o, --output  output directory');
+    console.log();
+    console.log('    -d, --dry-run  don\'t actually download the files');
+    console.log();
+    console.log('    -v, --version  get the current pulldown version');
+    console.log();
+    console.log('    -n, --noisy  verbose mode, basically');
+    console.log();
+    console.log('  Example usage:');
+    console.log();
+    console.log('    pulldown jquery             # Downloads jQuery');
+    console.log('    pulldown jquery::jq.js      # Downloads jQuery to jq.js');
+    console.log('    pulldown jquery angular.js  # Downloads jQuery and Angular.js');
+    console.log('    pulldown backbone           # Downloads jQuery, Underscore.js and Backbone.js');
+    console.log('    pulldown backbone -o js     # Downloads same as above, but into js/');
+    console.log();
+  },
   parseArgs: function(args) {
     var libraryArgs = args._;
     if(args.v || args.version) return console.log(pkg.version);
-    if(args.h || args.help || !libraryArgs.length) return pulldown.help();
+    if(args.h || args.help || !libraryArgs.length) return this.help();
     if(libraryArgs[0] == "ls") {
       return pulldown.ls(function(data) {
         data.forEach(function(item) {
